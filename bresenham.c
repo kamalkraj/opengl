a6cd40ed736866d6e4639b872d79cf803c654aa7
kamalraj <kamalraj97@gmail.com>
2016-08-07 15:39:14 +0530

    corrected speeling

--- bresenham.c
+++ bresenham.c
@@ -55,7 +55,7 @@ int main(int argc, char **argv) {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(500, 500);
   glutInitWindowPosition(0, 0);
-  glutCreateWindow("bresenhsm line");
+  glutCreateWindow("bresenham line");
   init();
   glutDisplayFunc(drawmyline);
   glutMainLoop();
